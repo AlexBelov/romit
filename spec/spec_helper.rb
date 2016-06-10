95dfea1fa4d46cadde7d2a72e651e5a249bcb8b2
Alexander Belov <git@belov.by>
2016-06-10 20:26:56 +0300

    Fix VCR config to allow results reporting to codeclimate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,7 @@ require 'webmock'
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/vcr_cassettes'
   config.hook_into :webmock
+  config.ignore_hosts 'codeclimate.com'
 end
 
 module Minitest
